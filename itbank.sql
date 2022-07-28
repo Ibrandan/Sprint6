e56665c45a05c1d7c917f93779d9f7cd9e2ec798
Ignacio Brandán <brandanignaciod@gmail.com>
2022-07-28 18:30:24 -0300

    Insercion de comentarios

--- itbank.sql
+++ itbank.sql
@@ -1,14 +1,28 @@
 -- Problematica 2
 
+/* Vista con las columnas id, numero sucursal, nombre, apellido, DNI 
+y edad de la tabla cliente calculada a partir de la fecha de nacimiento */
+
 CREATE VIEW listado_clientes
 as
 SELECT
 customer_id, branch_id,customer_name, customer_surname, customer_DNI, strftime('%Y',date('now')) - strftime('%Y',date(dob)) AS 'customer_age'
 FROM cliente;
 
+/* strftime() devuelve la fecha formateada como se indica en el primer argumento. El segundo parámetro se usa para mencionar la cadena de tiempo, pueder ser la fecha actual, fecha de nacimiento, etc. */
+
+/* Mostrar las columnas de los clientes, ordenadas por el DNI de menor 
+a mayor y cuya edad sea superior a 40 años */
+
 SELECT * from listado_clientes WHERE customer_age > 40 ORDER by customer_DNI;
 
+/*  Mostrar todos los clientes que se llaman “Anne” o “Tyler” ordenados 
+por edad de menor a mayor*/
+
 SELECT * FROM listado_clientes WHERE customer_name = 'Anne' or customer_name = 'Tyler' ORDER by customer_age;
+
+/* Insertar 5 nuevos clientes en la base de datos y 
+verificar que se haya realizado con éxito la inserción */
 
 INSERT INTO cliente(customer_name, customer_surname, customer_DNI, branch_id, dob)
 VALUES('Lois', 'Stout', '47730534', 80,'1984-07-07');
@@ -31,16 +45,29 @@ SELECT * FROM listado_clientes WHERE customer_DNI = '43625213';
 SELECT * FROM listado_clientes WHERE customer_DNI = '21207908';
 SELECT * FROM listado_clientes WHERE customer_DNI = '57063950';
 
+/*  Actualizar 5 clientes recientemente agregados en la base de datos dado que 
+hubo un error en el JSON que traía la información, la sucursal de todos es 
+la 10 */
+
 UPDATE cliente set branch_id = 10 WHERE customer_DNI = '47730534';
 UPDATE cliente set branch_id = 10 WHERE customer_DNI = '52055464';
 UPDATE cliente set branch_id = 10 WHERE customer_DNI = '43625213';
 UPDATE cliente set branch_id = 10 WHERE customer_DNI = '21207908';
 UPDATE cliente set branch_id = 10 WHERE customer_DNI = '57063950';
 
+/* Eliminar el registro correspondiente a “Noel David” realizando la selección 
+por el nombre y apellido */
+
 DELETE FROM cliente WHERE customer_name = 'Noel' AND customer_surname = 'David';
 
+/*  Consultar sobre cuál es el tipo de préstamo de mayor importe */
+
+-- Primer opción
 SELECT loan_type, max(loan_total) FROM prestamo;
 
+/* MAX() es una función que devuelve el valor máximo en un conjunto de registros.*/
+
+-- Segunda opción
 SELECT * from prestamo ORDER by loan_total DESC LIMIT 1; 
 
 -- Problematica 3
