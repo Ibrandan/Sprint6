94c0b0e2bbeac234e51a5955a6485b88a24ada7f
Ignacio Brandán <brandanignaciod@gmail.com>
2022-07-28 19:20:54 -0300

    Corrección de format

--- itbank.sql
+++ itbank.sql
@@ -100,14 +100,12 @@ SET tipo = (
         FROM tipo_cliente
         WHERE client_id = cliente.customer_id
     );
+
 -- Problematica 2
 /* Vista con las columnas id, numero sucursal, nombre, apellido, DNI 
  y edad de la tabla cliente calculada a partir de la fecha de nacimiento */
-/*
- Crear en la base de datos los tipos de cliente, de cuenta y marcas de 
- tarjeta. Insertar los valores según la información provista en el Sprint 
- 5 
- */
+
+
 -- Problematica 2
 CREATE VIEW listado_clientes as
 SELECT customer_id,
@@ -117,22 +115,28 @@ SELECT customer_id,
     customer_DNI,
     strftime('%Y', date('now')) - strftime('%Y', date(dob)) AS 'customer_age'
 FROM cliente;
+
 /* strftime() devuelve la fecha formateada como se indica en el primer argumento. El segundo parámetro se usa para mencionar la cadena de tiempo, pueder ser la fecha actual, fecha de nacimiento, etc. */
 /* Mostrar las columnas de los clientes, ordenadas por el DNI de menor 
  a mayor y cuya edad sea superior a 40 años */
+
 SELECT *
 from listado_clientes
 WHERE customer_age > 40
 ORDER by customer_DNI;
+
 /*  Mostrar todos los clientes que se llaman “Anne” o “Tyler” ordenados 
  por edad de menor a mayor*/
+
 SELECT *
 FROM listado_clientes
 WHERE customer_name = 'Anne'
     or customer_name = 'Tyler'
 ORDER by customer_age;
+
 /* Insertar 5 nuevos clientes en la base de datos y 
  verificar que se haya realizado con éxito la inserción */
+
 INSERT INTO cliente(
         customer_name,
         customer_surname,
@@ -141,6 +145,7 @@ INSERT INTO cliente(
         dob
     )
 VALUES('Lois', 'Stout', '47730534', 80, '1984-07-07');
+
 INSERT INTO cliente(
         customer_name,
         customer_surname,
@@ -155,6 +160,7 @@ VALUES(
         45,
         '1968-04-30'
     );
+
 INSERT INTO cliente(
         customer_name,
         customer_surname,
@@ -163,6 +169,7 @@ INSERT INTO cliente(
         dob
     )
 VALUES('Hilel', 'Mclean', '43625213', 77, '1993-03-28');
+
 INSERT INTO cliente (
         customer_name,
         customer_surname,
@@ -171,6 +178,7 @@ INSERT INTO cliente (
         dob
     )
 VALUES('Jin', 'Cooley', '21207908', 96, '1959-08-24');
+
 INSERT INTO cliente(
         customer_name,
         customer_surname,
@@ -185,55 +193,65 @@ VALUES(
         27,
         '1976-04-01'
     );
+
 SELECT *
 FROM listado_clientes
 WHERE customer_DNI = '47730534';
+
 SELECT *
 FROM listado_clientes
 WHERE customer_DNI = '52055464';
+
 SELECT *
 FROM listado_clientes
 WHERE customer_DNI = '43625213';
+
 SELECT *
 FROM listado_clientes
 WHERE customer_DNI = '21207908';
+
 SELECT *
 FROM listado_clientes
 WHERE customer_DNI = '57063950';
+
 /*  Actualizar 5 clientes recientemente agregados en la base de datos dado que 
  hubo un error en el JSON que traía la información, la sucursal de todos es 
  la 10 */
+
 UPDATE cliente
 set branch_id = 10
 WHERE customer_DNI = '47730534';
+
 UPDATE cliente
 set branch_id = 10
 WHERE customer_DNI = '52055464';
+
 UPDATE cliente
 set branch_id = 10
 WHERE customer_DNI = '43625213';
+
 UPDATE cliente
 set branch_id = 10
 WHERE customer_DNI = '21207908';
+
 UPDATE cliente
 set branch_id = 10
 WHERE customer_DNI = '57063950';
+
 /* Eliminar el registro correspondiente a “Noel David” realizando la selección 
  por el nombre y apellido */
+
 DELETE FROM cliente
 WHERE customer_name = 'Noel'
     AND customer_surname = 'David';
+
 /*  Consultar sobre cuál es el tipo de préstamo de mayor importe */
 -- Primer opción
 SELECT loan_type,
     max(loan_total)
 FROM prestamo;
 /* MAX() es una función que devuelve el valor máximo en un conjunto de registros.*/
--- Segunda opción
-SELECT *
-from prestamo
-ORDER by loan_total DESC
-LIMIT 1;
+
 -- Problematica 3
 --Punto 1
 SELECT account_id,
